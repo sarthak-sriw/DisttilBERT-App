1766cffa9c31e77bd6906c8c3f76805f4b708c21
Sarthak Srivastava <sarthak.btech.ec17@iiitranchi.ac.in>
2020-08-20 00:23:27 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import Flask
+from flask import request,jsonify
+from flask_ngrok import run_with_ngrok
+import ktrain
 
 app = Flask(__name__)
+#run_with_ngrok(app)  # Start ngrok when app is run
 
-@app.route('/')
-def hello():
-    return "conraqs server is working"
+predictor = ktrain.load_predictor('distilbert')
+
+def get_prediction(x):
+    sent=predictor.predict([x])
+    return sent[0]
+
+# for / root, return Hello Word
+@app.route("/")
+def root():
+    return f"Congrats! EC2 Flask Server is working"
+
+@app.route('/get_sentiment',methods=['POST'])
+def get_sentiment():
+    tx = request.get_json(force=True)
+    text=tx['Review']
+    sent=get_prediction(text)
+    return jsonify(result = sent)
 
 
-if __name__ == '__main__':
-    app.run(host = '0.0.0.0', debug=True,port = 5000)
-    
+app.run()
